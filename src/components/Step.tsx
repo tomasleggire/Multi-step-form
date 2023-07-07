925d44c35d4ba2b509cb85bfcc78c85890251008
tomasleggire <tomasleggire@hotmail.com>
2023-07-07 16:37:06 -0300

    Steps maquetado

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -8,9 +8,12 @@ interface MyProps {
 
 export default function Step(props: MyProps) {
   return (
-    <div>
-      <h1>{props.num}</h1>
-      <h1>{props.title}</h1>
+    <div className="main-step">
+      <h4 className="step-num">{props.num}</h4>
+      <div className="step-text">
+        <h6 className="step-text-title">Step {props.num}</h6>
+        <h5 className="step-text-subtitle">{props.title}</h5>
+      </div>
     </div>
   );
 }
